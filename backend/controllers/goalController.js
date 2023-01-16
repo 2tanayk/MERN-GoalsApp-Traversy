b9a6a07e3024568cc6c3d41034c072bc27709154
Tanay Kamath <2tanaykamath@gmail.com>
2023-01-17 02:44:42 +0530

    basic CRUD api created

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,62 @@
-const asyncHandler=require('express-async-handler')
+const asyncHandler = require("express-async-handler");
+
+const Goal = require("../models/goalModel");
+const { text } = require("body-parser");
 
 const getGoals = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Get goals" });
-})
+  const goals = await Goal.find();
+
+  res.status(200).json(goals);
+});
 
 const setGoal = asyncHandler(async (req, res) => {
-  console.log(req.body)  
+  console.log(req.body);
 
-  if(!req.body.text){
+  if (!req.body.text) {
     //res.status(400).json({message:'Please add a text field'})
-    res.status(400)
+    res.status(400);
     //error handler will gives us a HTML page indicating the error that occured
-    throw new Error('Please add a text field')
+    throw new Error("Please add a text field");
   }
-  res.status(200).json({ message: "Set goals" });
-})
 
-const updateGoal =  asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Update goal ${req.params.id}` });
-})
+  const goal = await Goal.create({
+    text: req.body.text,
+  });
+
+  res.status(200).json(goal);
+});
+
+const updateGoal = asyncHandler(async (req, res) => {
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json(updatedGoal);
+});
 
 const deleteGoal = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: `Delete goal ${req.params.id}` });
-})
+  const goal = await Goal.findById(req.params.id);
+
+  if (!goal) {
+    res.status(400);
+    throw new Error("Goal not found");
+  }
+
+  await goal.remove();
+
+  res.status(200).json({ id: req.params.id });
+});
 
 module.exports = {
   getGoals,
   setGoal,
   updateGoal,
-  deleteGoal
-}
+  deleteGoal,
+};
