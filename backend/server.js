23ba149c5b59c5f7d28f64f52c9d53eeceed5faa
Tanay Kamath <2tanaykamath@gmail.com>
2023-01-17 02:14:14 +0530

    connect with database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const colors=require('colors')
 const {errorHandler}=require("./middleware/errorMiddleware")
+const connectDb=require('./config/db')
 const port = process.env.PORT || 5000;
+
+connectDb()
 
 const app = express();
 
