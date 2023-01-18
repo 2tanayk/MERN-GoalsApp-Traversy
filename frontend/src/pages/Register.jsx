267a77c186b692bea31e2e22a24719e0161db847
Tanay Kamath <2tanaykamath@gmail.com>
2023-01-19 03:08:07 +0530

    create register function

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import { FaUser } from "react-icons/fa";
+import { register, reset } from "../features/auth/authSlice";
+import Spinner from "../components/Spinner";
 
 function Register() {
   const [formData, setFormData] = useState({
@@ -12,16 +17,47 @@ function Register() {
 
   const { name, email, password, passwordConfirm } = formData;
 
+  const navigate =useNavigate()
+  const dispatch=useDispatch()
+
+  const {user,isLoading,isError,isSuccess,message}=useSelector((state)=>state.auth)
+
+  useEffect(()=>{
+    if(isError){
+        toast.error(message)
+    }
+
+    if(isSuccess || user){
+        navigate('/')
+    }
+
+    dispatch(reset())
+  },[user,isError,isSuccess,message,navigate,dispatch])
+
   const onChange = (e) => {
-    setFormData((prevState)=>({
-        ...prevState,
-        [e.target.name]:e.target.value
-    }))
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
   };
 
   const onSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
+
+    if(password!==passwordConfirm){
+        toast.error('Passwords don\'t match')
+    }else{
+        const userData={
+            name,email,password
+        }
+
+        dispatch(register(userData))
+    }
   };
+
+  if(isLoading){
+    return <Spinner/>
+  }
 
   return (
     <>
